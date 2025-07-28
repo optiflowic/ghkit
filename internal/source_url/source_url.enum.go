0d1e53616dd8e2fb7869d1fbc052e45326eb9a2b
Hisanari Kikuchi <hisanari.kikuchi.dev@gmail.com>
2025-07-28 20:26:09 +0900

    feat: define enum for source_url

---
+++ internal/source_url/source_url.enum.go
@@ -0,0 +1,8 @@
+package source_url
+
+type SourceUrl string
+
+const (
+	Repository SourceUrl = "https://github.com/optiflowic/ghkit"
+	Templates  SourceUrl = "https://raw.githubusercontent.com/optiflowic/github-templates/main"
+)
